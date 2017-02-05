18d12ede26dc44d96b2f937c53f68f45f0f061fe
mk-pmb <mk-pmb@users.noreply.github.com>
2017-02-05 02:43:17 +0100

    v0.1.3: use MSIE test page for Firefox as well

--- test/all.js
+++ test/all.js
@@ -3,6 +3,14 @@
 'use strict';
 
 try { require('usnam-pmb'); } catch (ignore) {}
-require('./usage.js')();
 
+function dare(testName, func) {
+  try {
+    return func();
+  } catch (err) {
+    console.log('-ERR ' + testName + ' test failed: ', err);
+  }
+}
+
+dare('usage', require('./usage.js'));
 console.log("+OK all tests passed.");   //= "+OK all tests passed."
